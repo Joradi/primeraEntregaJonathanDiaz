0d4fc8a7bc964cefcbead9e0b85401779642e1a2
Jonathan Díaz <jona.diaz.m@gmail.com>
2023-03-29 21:19:04 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Navbar from './components/Navbar/Navbar.jsx';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer.jsx';
+
+function App() {
+  return (
+    <>
+    <Navbar />
+    <ItemListContainer greeting="Bienvenido"/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import CardWidget from '../CardWidget/CardWidget'
+
+const navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">Café del Puerto</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon" />
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Productos</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">Servicios</a>
+              </li>
+            </ul>
+            <CardWidget />
+          </div>
+        </div>
+      </nav>
+  )
+}
+
+export default navbar
